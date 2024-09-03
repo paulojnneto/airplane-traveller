1108fb1c58a9a6931d6d9dc9840f5374302cc01f
Paulo José Neves Nto <paulo.jose.n@outlook.com>
2024-09-03 20:06:45 -0300

    Added obstacled and coin

--- Main.gd
+++ Main.gd
@@ -1,7 +1,9 @@
 extends Node
 
 @export var obstacle : PackedScene;
+@export var coin : PackedScene;
 
+var last_obstacle_position : String;
 var dynamic_object_speed : int = 700;
 var spawned_object_position_x : int = 1700;
 var health_decrease_speed : int = 3;
@@ -25,16 +27,30 @@ func _process(delta):
 
 func _on_spawner_timer_timeout():
 	var random : int = randi() % 2;
-	var timer : float = randf();
-	var timer_node = get_node("SpawnerTimer");
-	timer_node.wait_time = timer;
 	var obstacle_instance : Area2D = obstacle.instantiate();
 	add_child(obstacle_instance);
-	obstacle_instance.add_to_group("DynamicObject")
 	obstacle_instance.position.x = spawned_object_position_x;
 	
 	if random == 0:
 		obstacle_instance.position.y = 200;
+		last_obstacle_position = "up"
 	else:
 		obstacle_instance.position.y = 800;
 		obstacle_instance.scale.y *= -1;
+		last_obstacle_position = "down"
+
+
+func _on_coin_timer_timeout():
+	var random_position : int = randi_range(100, 700);
+	var coin_instance : Area2D = coin.instantiate();
+	if last_obstacle_position == "up" && random_position <= 460:
+		return;
+	if last_obstacle_position == "down" && random_position >= 500:
+		return
+	add_child(coin_instance);
+	coin_instance.position.x = spawned_object_position_x
+	coin_instance.position.y = random_position;
+	coin_instance.body_entered.connect(_on_coin_collided.bind(coin_instance))
+
+func _on_coin_collided(body : Node2D) -> void:
+	pass
